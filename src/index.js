3ad6f4a732d510282656e495998f74eb7705bd4d
William Sinn <w.h.sinn@gmail.com>
2019-05-30 11:38:38 -0400

    cleaned up and noted code

--- src/index.js
+++ src/index.js
@@ -2,28 +2,30 @@ const BEERS_URL = "http://localhost:3000/beers"
 const listGroup = document.querySelector("#list-group")
 const beerDetail = document.querySelector("#beer-detail")
 
+//DELI-1
 fetch(BEERS_URL)
   .then(r => r.json())
   .then(beersObject => {
-      console.log(beersObject)
-      beersObject.forEach(beer => {
-        listGroup.innerHTML+= `
+    console.log(beersObject)
+    beersObject.forEach(beer => {
+      listGroup.innerHTML += `
           <li class="list-group-item" id=${beer.id}> ${beer.name}</li>
           `
+  })
+})//FETCH list-of-beer and append to DOM, DELI-1
 
-          })
-      })
 
-  listGroup.addEventListener("click", event => {
-    console.log(event.target)
-    let targetId = event.target.id
-    const BEER_URL = `http://localhost:3000/beers/${targetId}`
+//DELI-2
+listGroup.addEventListener("click", event => {
+  // console.log(event.target)
+  let targetId = event.target.id
+  const BEER_URL = `http://localhost:3000/beers/${targetId}`
 
-    fetch(BEER_URL)
-      .then(r => r.json())
-      .then(beerObj => {
-        console.log(beerObj)
-        beerDetail.innerHTML=`
+  fetch(BEER_URL)
+    .then(r => r.json())
+    .then(beerObj => {
+      // console.log(beerObj)
+      beerDetail.innerHTML = `
         <h1>${beerObj.name}</h1>
         <img src="${beerObj.image_url}">
         <h3>${beerObj.tagline}</h3>
@@ -32,25 +34,26 @@ fetch(BEERS_URL)
           Save
         </button>
         `
-        const editBeer = document.querySelector('#edit-beer')
-        console.log(editBeer)
-        editBeer.addEventListener('click', event => {
-            console.log(event.target)
-            let targetValue = event.target.previousElementSibling.value
-            fetch(BEER_URL, {
-              method:"PATCH",
-              headers:   {
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-                },
-                body: JSON.stringify(
-                  {description: targetValue}
-                )
-            }).then(r => r.json())
-              .then(console.log)
-            // debugger
-          })
+//DELI-2
 
+//DELI-3
+      const editBeer = document.querySelector('#edit-beer')
+      // console.log(editBeer)
+      editBeer.addEventListener('click', event => {
+        console.log(event.target)
+        let targetValue = event.target.previousElementSibling.value
+        fetch(BEER_URL, {
+            method: "PATCH",
+            headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+              description: targetValue
+            })
+          }).then(r => r.json())
+          // .then(console.log)
+        // debugger
       })
-
+    })
   })
